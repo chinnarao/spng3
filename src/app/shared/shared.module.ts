4d70502b78453aa4962a639e413804b93b31c40a
chinnarao <chinnarao@live.com>
2018-11-07 00:06:02 -0800

    flex layout center fixed, very tough one fixed, version 6, great copy

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,12 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { ReactiveFormsModule } from '@angular/forms';
-import { SpinnerComponent } from './components/spinner/spinner.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { HomeComponent } from './components/home/home.component';
-import { ErrorComponent } from './pages/error/error.component';
 import { UtilsService } from './helpers/utils.service';
-import { MdcModule } from './modules/mdc.module';
 
 @NgModule({
   imports: [
@@ -16,19 +11,10 @@ import { MdcModule } from './modules/mdc.module';
     RouterModule,
     FlexLayoutModule,
     ReactiveFormsModule,
-    MdcModule
   ],
   declarations: [
-    FooterComponent,
-    HomeComponent,
-    SpinnerComponent,
-    ErrorComponent,
   ],
   exports: [
-    FooterComponent,
-    HomeComponent,
-    SpinnerComponent,
-    ErrorComponent,
   ],
   providers: [UtilsService],
 })
