f86fd6f5c18077470f94364d8b2ee5b1aadb6b49
chinnarao <chinnarao@live.com>
2018-12-29 11:37:10 -0800

    cleaned, build --prod, success

--- src/app/_directives/time-before.directive.ts
+++ src/app/_directives/time-before.directive.ts
@@ -1,29 +1,29 @@
-import { ValidatorFn, AbstractControl, NG_VALIDATORS, Validator } from "@angular/forms";
-import { Directive, Input } from "@angular/core";
-import { isNull } from "@angular/compiler/src/output/output_ast";
+// import { ValidatorFn, AbstractControl, NG_VALIDATORS, Validator } from "@angular/forms";
+// import { Directive, Input } from "@angular/core";
+// import { isNull } from "@angular/compiler/src/output/output_ast";
 
-// created by Amit Baghel
-export function timebeforeValidator(dateCtrl:string): ValidatorFn {
+// // created by Amit Baghel
+// export function timebeforeValidator(dateCtrl:string): ValidatorFn {
 
-    return (control: AbstractControl): { [key: string]: any } | null => {
-        if (!control.value)
-            return null
+//     return (control: AbstractControl): { [key: string]: any } | null => {
+//         if (!control.value)
+//             return null
             
-        var date= control['parent'].value[dateCtrl].split('/');
-        var fullDate=date[2]+"-"+date[0]+"-"+date[1]+"T"+control.value.split('T')[1];
-        var dateVar=new Date(fullDate)
-        // let hour=parseInt(control.value.split(':')[0])
-        return dateVar <= new Date() ? {'timebefore':true} : null;
-    };
-}
+//         var date= control['parent'].value[dateCtrl].split('/');
+//         var fullDate=date[2]+"-"+date[0]+"-"+date[1]+"T"+control.value.split('T')[1];
+//         var dateVar=new Date(fullDate)
+//         // let hour=parseInt(control.value.split(':')[0])
+//         return dateVar <= new Date() ? {'timebefore':true} : null;
+//     };
+// }
 
 
-@Directive({
-    selector: '[timebefore]',
-    providers: [{ provide: NG_VALIDATORS, useExisting: TimeBeforeValidatorDirective, multi: true }]
-})
-export class TimeBeforeValidatorDirective implements Validator {
-    @Input('timebefore') timebefore: string;
+// @Directive({
+//     selector: '[timebefore]',
+//     providers: [{ provide: NG_VALIDATORS, useExisting: TimeBeforeValidatorDirective, multi: true }]
+// })
+// export class TimeBeforeValidatorDirective implements Validator {
+//     @Input('timebefore') timebefore: string;
 
-    validate(control: AbstractControl): { [key: string]: any } | null { return timebeforeValidator(this.timebefore)(control); }
-}
+//     validate(control: AbstractControl): { [key: string]: any } | null { return timebeforeValidator(this.timebefore)(control); }
+// }
